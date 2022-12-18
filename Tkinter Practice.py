98634969c3d55a50911c582564da22a4d0690a22
krafay412 <93526193+krafay412@users.noreply.github.com>
2022-12-19 01:03:02 +0500

    Add files via upload

---
+++ Tkinter Practice.py
@@ -0,0 +1,42 @@
+import turtle
+import random
+
+Screen = turtle.getscreen()
+Screen.bgcolor("yellow")
+
+Bob = turtle.Turtle()
+Colors = ["red","green","cyan","blue","purple","orange","black"]
+Bob.speed(10)
+
+for i in range(5):
+
+    Bob.color(Colors[random.randint(0,6)],Colors[random.randint(0,6)])
+    Bob.begin_fill()
+    Bob.circle(10)
+    Bob.end_fill()
+    Bob.color(Colors[random.randint(0,6)],Colors[random.randint(0,6)])
+    Bob.begin_fill()
+    Bob.circle(-10)
+    Bob.end_fill()
+    Bob.color(Colors[random.randint(0,6)],Colors[random.randint(0,6)])
+    Bob.left(90)
+    Bob.begin_fill()
+    Bob.circle(10)
+    Bob.end_fill()
+    Bob.color(Colors[random.randint(0,6)],Colors[random.randint(0,6)])
+    Bob.begin_fill()
+    Bob.circle(-10)
+    Bob.end_fill()
+
+    Bob.color(Colors[random.randint(0,6)],Colors[random.randint(0,6)])
+    Bob.begin_fill()
+    Bob.forward(300)
+    Bob.left(126.7)
+    Bob.end_fill()
+
+
+
+
+
+
+turtle.mainloop()
